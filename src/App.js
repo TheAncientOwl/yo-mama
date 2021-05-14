7b4b59f0e8c1dae4afaed437bb962e96be0fefb8
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2021-05-15 00:05:32 +0300

    added joke and new joke button.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import randomJoke from './jokes/randomJoke';
+import { Button } from '@material-ui/core';
 
+const jk = randomJoke();
 export default function App() {
+  const [joke, setJoke] = useState(jk);
+
+  const handleNewJoke = () => setJoke(randomJoke());
+
   return (
-    <div
-      style={{
-        textAlign: 'center',
-        marginTop: '20vh',
-        fontSize: '5vw',
-        fontStyle: 'italic',
-        color: 'red',
-        fontWeight: 'bold',
-      }}>
-      {randomJoke()}
-    </div>
+    <React.Fragment>
+      <Button onClick={handleNewJoke} color='secondary' variant='outlined'>
+        NEW JOKE
+      </Button>
+      <div
+        style={{
+          textAlign: 'center',
+          marginTop: '20vh',
+          fontSize: '5vw',
+          fontStyle: 'italic',
+          color: 'red',
+          fontWeight: 'bold',
+        }}>
+        {joke}
+      </div>
+    </React.Fragment>
   );
 }
